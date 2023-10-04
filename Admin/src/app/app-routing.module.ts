c9ac0e897b6d7e31435ae04cf167e26a32519022
ajay-gupta25 <ajaykumar.gupta@etechtexas.com>
2023-10-04 15:28:24 +0530

    Maco page and navigation added for MyZoneMyDevice

--- Admin/src/app/app-routing.module.ts
+++ Admin/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { Page404Component } from './extrapages/page404/page404.component';
 const routes: Routes = [
   { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
   // tslint:disable-next-line: max-line-length
+  // { path: '',  loadChildren: () => import('./account/account.module').then(m => m.AccountModule), canActivate: [AuthGuard] },
   { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },
   { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule), canActivate: [AuthGuard] },
   { path: 'crypto-ico-landing', component: CyptolandingComponent },
